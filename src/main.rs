807d0d331b263398ac42b147f5a9fd4bf01d9d32
erwanor <erwan.ounn.84@gmail.com>
2022-06-06 21:12:31 +0200

    build packet and send buffer

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::net::{IpAddr, Ipv4Addr};
-use etherparse::{Ipv4HeaderSlice};
+use etherparse::Ipv4HeaderSlice;
 use icmp::{self, IcmpSocket};
-use packet::builder::Builder;
+use packet::builder::{Builder, Finalizer};
 use packet::icmp as icmp2;
+use std::net::{IpAddr, Ipv4Addr};
 
 struct IcmpHeader {
     msg: u8,
@@ -24,18 +24,26 @@ impl IcmpHeader {
 
 fn main() {
     println!("Hello, world!");
-    let localhost = IpAddr::V4(Ipv4Addr::new(139, 178, 84, 217 ));
+    let localhost = IpAddr::V4(Ipv4Addr::new(139, 178, 84, 217));
     let mut socket = icmp::IcmpSocket::connect(localhost).expect("failed to bind local socket");
     let request = IcmpHeader::ping();
     let mut b: [u8; 8] = [0; 8];
     b[0] = 8;
     b[2] = 8;
 
-    let builder = icmp2::Builder::default();
-    let echo_builder = builder.echo().expect("Failed to build echo");
-    let packet = echo_builder.with(b).expect("Failed to build packet");
+    let mut builder = icmp2::Builder::default();
 
-    socket.send(&b);
+    let mut echo_builder = builder.echo().expect("Failed to build echo");
+
+    let mut echo_req_builder = echo_builder
+        .request()
+        .expect("failed to create ICMP echo request");
+
+    let mut echo_req_final = echo_req_builder.identifier(42 as u16).unwrap();
+    // let mut final_req = echo_req_final.payload(b"").unwrap();
+    let p = echo_req_final.build().expect("failed to build!!");
+
+    socket.send(p.as_slice());
 
     let mut b2: [u8; 32] = [0; 32];
 
